b62c1171ef51fe812dc99b3aad718c2bd997e2e0
Darren Tan <50022686+Renkuro@users.noreply.github.com>
2021-05-02 23:35:51 +0800

    Add files via upload

---
+++ CSOPESY-Project1-S15-Group16.c
@@ -0,0 +1,71 @@
+/***************************************************************
+Names: TAN, DARREN
+       OAFALLAS, KENNETH
+Group: 16
+Section: S15
+***************************************************************/
+
+
+#include <stdio.h>
+
+#include "queue.h"
+
+
+typedef struct Processes {
+	int id;
+	int arrival_time;
+	int total_exec_time;
+	int burst_time_length;
+	int often;
+} Process;
+
+void 
+priorityBoost(struct Queue queuelist[], int nQueues)
+{
+	int i, element;
+	
+	for(i = 0; i < nQueues; i++)
+	{
+		element = dequeue(&queuelist[i + 1]);
+		enqueue(element, &queuelist[0]);
+	}
+}
+
+int
+main()
+{
+	char filename[50], txt[] = ".txt";
+    int nQueues, nProcesses, time_period;
+    
+
+    printf("Enter file name: ");
+    scanf("%s", filename);
+	printf("\n");
+	
+    strcat(filename, txt);
+
+    FILE* fp = fopen(filename, "r");
+
+    if(fp == NULL)
+    {
+        printf("%s not found.", filename);
+        return 0;
+    }
+    
+    fscanf(fp, "%d %d %d", &nQueues, &nProcesses, &time_period); 
+	
+	
+	struct Queue queuelist[nQueues]; // Queues
+    Process processlist[nProcesses]; // Processes
+	int i;
+	
+	for(i = 0; i < nQueues; i++)
+		fscanf(fp, "%d %d %d", queuelist[i].id, queuelist[i].priority, queuelist[i].quantum); 
+	
+	for(i = 0; i < nProcesses; i++)
+		fscanf(fp, "%d %d %d %d %d", processlist[i].id, processlist[i].arrival_time, processlist[i].total_exec_time, processlist[i].burst_time_length, processlist[i].often); 
+	
+	priorityBoost(queuelist, nQueues);
+	
+	return 0;
+}
---
+++ queue.h
@@ -0,0 +1,54 @@
+/***********************************************
+Names:  TAN, Darren
+        OAFALLAS, Kenneth Neil
+Group: 16
+Section: S15
+***********************************************/
+struct Queue
+{
+    int *elements;
+    int size;
+    int max;
+    int head;
+    int tail;
+    int id;
+	int priority;
+	int quantum;
+};
+
+void initQueue(int n, struct Queue *q)
+{
+	q->elements = (int*)malloc(n * sizeof(int));
+	q->max = n;
+	q->size = 0;
+    q->head = 0;
+    q->tail = n-1;
+}
+
+int peek(struct Queue *q)
+{
+	if(q->size <= 0) return -1;
+	//printf("%d\n",q->elements[q->head]);
+    return q->elements[q->head];
+}
+
+int dequeue(struct Queue *q)
+{
+        if(q->size <= 0) return -1;
+        
+        int element;
+        element = q->elements[q->head];
+        q->head = (q->head+1) % q->max;
+        q->size--;
+        //printf("DQ: %d, Size: %d, T: %d, H: %d\n", element, q->size, q->tail, q->head);
+        return element;
+}
+
+void enqueue(int element, struct Queue *q)
+{
+		if(q->size >= q->max) return;
+        q->tail = (q->tail+1) % q->max;
+        q->elements[q->tail] = element;
+        q->size++;
+        //printf("NQ: %d, Size: %d, T: %d, H: %d\n", q->elements[q->tail], q->size, q->tail, q->head);
+}
